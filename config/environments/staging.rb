dc6c371a7bfd30f315449c3a583a504e5ed5d933
Ondřej Ezr <oezr@msp.justice.cz>
2017-12-20 14:27:17 +0100

    sidekiq capfile, mailer config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: '172.31.225.23' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'Servisex03.servis.justice.cz',
+    port:                 25,
+    domain:               'justice.cz',
+    # user_name:            '<username>',
+    # password:             '<password>',
+    # authentication:       'plain',
+    enable_starttls_auto: true  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
