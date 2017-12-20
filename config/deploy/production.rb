d6ccbcb5147bf9298682d2444cca4d635c199cc5
Ondřej Ezr <oezr@msp.justice.cz>
2017-12-20 01:30:05 +0100

    deploy to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,16 @@
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+server "serviscek01.servis.justice.cz", user: ENV['PRODUCTION_USER_NAME'], roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
@@ -31,7 +39,7 @@
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
 
-
+set :unicorn_config_path, File.join(current_path, "config", "unicorn.rb")
 
 # Custom SSH Options
 # ==================
@@ -41,12 +49,12 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
+set :ssh_options, {
+  keys: %w(~/.ssh/id_rsa),
+  forward_agent: false,
+  auth_methods: %w(publickey)
+}
+
 # The server-based syntax can be used to override options:
 # ------------------------------------
 # server "example.com",
