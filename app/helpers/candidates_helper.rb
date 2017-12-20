811788c56a4fcf05abd3a13714090f3f12be59bd
Ondřej Ezr <oezr@msp.justice.cz>
2017-12-20 12:56:59 +0100

    file not found message

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -4,7 +4,11 @@ module CandidatesHelper
     if candidate.diploma.ext == 'pdf'
       content_tag('i', '', class: 'fa fa-file-pdf-o fa-3x')
     else
-      image_tag(candidate.diploma.thumb('250x250#').url, alt: Candidate.human_attribute_name('diploma'))
+      begin
+        image_tag(candidate.diploma.thumb('250x250#').url, alt: Candidate.human_attribute_name('diploma'))
+      rescue
+        'File not found'
+      end
     end
   end
 
