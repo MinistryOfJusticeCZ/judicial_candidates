e2ed84f8abc3fc02a1b968e3cab6de02f68ba9cd
Ondřej Ezr <oezr@msp.justice.cz>
2018-03-22 15:14:33 +0100

    staging url

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: '172.31.225.23' }
+  config.action_mailer.default_url_options = { host: 'https://cekateletst.servis.justice.cz' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
