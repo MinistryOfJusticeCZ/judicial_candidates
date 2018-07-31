88f4ef96d6fd3bd22f279491d5978eb1ffacdb8c
Ondřej Ezr <oezr@msp.justice.cz>
2018-07-31 12:49:47 +0200

    node

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+ENV['EXECJS_RUNTIME'] ||= 'Node' unless Rails.env.test?
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,3 @@
-ENV['EXECJS_RUNTIME'] = nil
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
