f4f1145ec92691664b0c23d9968ca4cd69e4a70c
Ondřej Ezr <oezr@msp.justice.cz>
2018-07-31 12:27:19 +0200

    node

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,3 @@
-ENV['EXECJS_RUNTIME'] ||= 'Node'
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+ENV['EXECJS_RUNTIME'] = nil
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
