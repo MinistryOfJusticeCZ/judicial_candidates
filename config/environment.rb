bc825220c03c3b05fbbd70e052b7bd754460c81d
Ondřej Ezr <oezr@msp.justice.cz>
2018-07-31 13:58:29 +0200

    bundle with node

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-ENV['EXECJS_RUNTIME'] ||= 'Node' unless Rails.env.test?
-
 # Initialize the Rails application.
 Rails.application.initialize!
