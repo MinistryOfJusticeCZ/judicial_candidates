c54dd9b4b7e919a00c85114d83f498af4973c190
Ondřej Ezr <oezr@msp.justice.cz>
2018-08-20 16:33:47 +0200

    redefine previews directory

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "JudicialCandidates_#{Rails.env}"
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.preview_path  = "#{Rails.root}/spec/mailers/previews"
   config.action_mailer.show_previews = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
