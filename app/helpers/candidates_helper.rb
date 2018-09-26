c357bd4ac6333a58193856fe33e58c4dcbc54aaf
Ondřej Ezr <oezr@msp.justice.cz>
2018-09-26 12:49:46 +0200

    file icon - pdf

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -2,7 +2,7 @@ module CandidatesHelper
 
   def diploma_thumb(candidate)
     if candidate.diploma.ext == 'pdf'
-      content_tag('i', '', class: 'fa fa-file-pdf-o fa-3x')
+      content_tag('i', '', class: 'fa fa-file-pdf fa-3x')
     else
       image_tag(candidate.diploma.thumb('250x250#').url, alt: Candidate.human_attribute_name('diploma'))
     end
